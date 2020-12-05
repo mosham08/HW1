b5c55b7e574aa5d6c59197c56ea928d23529de99
Shamroukh, Emad <emad.shamroukh@sap.com>
2020-12-05 14:11:09 -0600

    more files to upload

--- HW_1/mytest.c
+++ HW_1/mytest.c
@@ -14,9 +14,18 @@ struct Disk {
     // is free bit
     unsigned free: 1;
     // add the file name later
+    //int *blocks;
 };
 struct Disk *disk[MAX];
 
+struct Directory {
+    char name[50];
+    int inode;
+};
+//void initBlocksArray(Disk *disk, size_t initialSize) {
+//    disk->blocks = malloc(initialSize * sizeof(int));
+//}
+int whereToStart = 0;
 void loadFileInDisk(const char *fileName) {
     char buffer[MAX_FILE_SIZE];
     printf("%4s: %s\n", "File", fileName);
@@ -26,22 +35,30 @@ void loadFileInDisk(const char *fileName) {
     if (file != NULL) {
         int ch;
         i = 0;
+        // the first step is to store the whole file data (bytes) in the memory INTERMEDIAte
         while ((ch = fgetc(file)) != EOF) {
             buffer[i++] = ch;
         }
         buffer[i++] = 0x00;
-        j = i;
+        j = i; // count of bytes in the file .
         fclose(file);
     }
 
+    // we want to know how many blocks needed
     printf("%4s: 11111111111111 %s\n", "File", fileName);
-    int size = sizeof(buffer) / sizeof(buffer[0]);
+    //int size = sizeof(buffer) / sizeof(buffer[0]);
     double value = (double) j / 256.0;
     int blocks = (int) ceil(value);
     printf("Size of the array: %ld  number of blocks: %d\n", j, blocks);
 
+
     index = 0; // to 256
-    int countBlocks = 0; // to number of blocks
+    int countBlocks = whereToStart; // to number of blocks. WE have to figure out the next available block
+
+    // initialize the blocks numbers array
+    //initBlocksArray(@disk[countBlocks], blocks+1);
+    // take every byte from the memory buffer and insert it into the corresponding block in the disk array
+    // this code stores the data into blocks
     for (i = 0; i < j; ++i) {
         if (buffer[i] == 0x00) break;
         // make sure that the data cell is free.
@@ -50,11 +67,15 @@ void loadFileInDisk(const char *fileName) {
         index++;
 
         if (index > 256) {
-            index = 0;
-            countBlocks++;
+            index = 0; // count characters
+            countBlocks++; // increment the block. means use the next available block
             printf("Next block %d \n", countBlocks);
         }
     }
+
+    // countBlocks = 4
+    // Next countBlocks = 5 //
+    whereToStart = countBlocks;
 
     printf("After loading files: %ld\n", index);
     for (int i1 = 0; i1 < blocks; i1++) {
@@ -92,14 +113,16 @@ int main() {
     }
 
     /* Read directory entries */
-    while ((entry = readdir(folder))) {
+    int count = 0;
+    while ((entry = readdir(folder)) && count < 2) {
         stat(entry->d_name, &filestat);
         if (S_ISDIR(filestat.st_mode))
             printf("%4s: %s\t%s\n", "Dir", entry->d_name, ctime(&filestat.st_ctime));
         else {
             printf("%4s: %s\t%s\n", "File", entry->d_name, ctime(&filestat.st_ctime));
             loadFileInDisk(entry->d_name);
-            break;
+            count ++;
+//            break;
         }
 
     }
